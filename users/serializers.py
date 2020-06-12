38642cfa5cd17e9aa8771770f8736679b44fa17f
SilasKenneth <silaskenneth1@gmail.com>
2020-06-12 17:46:27 +0300

    Create the serializer for users

---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+from .models import User, Student
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = User
+
+
+class StudentSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Student
+
+
+class SponsorSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Sponsor
